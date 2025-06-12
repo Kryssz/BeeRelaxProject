e52b2011cee3275ab4844ace8ae6d028e83b4ed2
Kryssz <kryssz77@gmail.com>
2025-06-12 16:31:55 +0200

    test_registration.py

--- Automated_tests/generate_browsers.py
+++ Automated_tests/generate_browsers.py
@@ -3,14 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-#
-# def generate_chrome_driver():
-#     options = ChromeOptions()
-#     options.add_argument('--disable-search-engine-choice-screen')
-#     options.add_experimental_option("detach", True)
-#     driver = webdriver.Chrome(options=options)
-#     driver.maximize_window()
-#     return driver
 
 def generate_chrome_driver():
     options = ChromeOptions()
