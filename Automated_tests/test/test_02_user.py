bc1abad7b69b8352bf69b6899d3962606cd021de
Kryssz <kryssz77@gmail.com>
2025-06-18 15:32:14 +0200

    test_user.py

--- Automated_tests/test/test_02_user.py
+++ Automated_tests/test/test_02_user.py
@@ -1,13 +1,11 @@
-from Automated_tests.generate_browsers import generate_chrome_driver
+from Automated_tests.generate_browsers import generate_chrome_driver_headless
 from Automated_tests.BeeRelaxed.page_model_user import LoginPage
 
 
-class TestPassword(object):
-    def __init__(self):
-        self.login_page = None
+class TestUserLogin(object):
 
     def setup_method(self):
-        driver = generate_chrome_driver()
+        driver = generate_chrome_driver_headless()
         self.login_page = LoginPage(driver)
         self.login_page.get()
 
@@ -16,9 +14,8 @@ class TestPassword(object):
 
 
     def test_user_registration(self):
-        page = LoginPage(self.driver)
-        page.bar_button_login().click()
-        page.input_username().send_keys('test@email.com')
-        page.input_password().send_keys('TestPW1!')
-        page.button_login_page().click()
-        assert page.bar_button_logout().is_displayed()
+        self.login_page.bar_button_login().click()
+        self.login_page.input_username().send_keys('test@email.com')
+        self.login_page.input_password().send_keys('TestPW1!')
+        self.login_page.button_login_page().click()
+        assert self.login_page.bar_button_logout().is_displayed()
